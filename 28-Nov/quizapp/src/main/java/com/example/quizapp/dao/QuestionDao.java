83e099dd43c680bf5fd61f055f4a1d7a66e03eee
Ananya1174 <157110426+Ananya1174@users.noreply.github.com>
2025-11-29 17:33:39 +0530

    Delete 28-Nov directory

--- 28-Nov/quizapp/src/main/java/com/example/quizapp/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.quizapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.quizapp.model.Question;
-
-public interface QuestionDao extends JpaRepository<Question,Integer>{
-
-}
